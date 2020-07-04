b244247d8fd4d8210e26064efe5040a99d1edcf6
ritzip <49394064+ritzip@users.noreply.github.com>
2020-07-04 20:48:03 +0800

    oof forgot to save

--- scripts/unitSpawner.js
+++ scripts/unitSpawner.js
@@ -1,4 +1,8 @@
 const unitSpawner = extendContent(Block, "unit-spawner", {
+    load(){
+        this.region = Core.atlas.find(this.name);
+        this.regionDraw = Core.atlas.find(this.name + "-draw");
+    },
 
     buildConfiguration(tile, table){
         table.addImageButton(Icon.wrench, Styles.clearTransi, run(() => {
@@ -13,7 +17,6 @@ const unitSpawner = extendContent(Block, "unit-spawner", {
             tile.configure(2);
         })).size(50).disabled(boolf(b => tile.entity == null));
     },
-
     pick(tile){
         const dialog = new FloatingDialog("");
         dialog.setFillParent(true);
@@ -21,7 +24,7 @@ const unitSpawner = extendContent(Block, "unit-spawner", {
             var i = 0;
             var units = Vars.content.units();
 
-            units.each(cons(type=>{
+            units.each(cons(type => {
                 p.addButton(cons(t => {
                     t.left();
                     t.addImage(type.icon(Cicon.medium)).size(40).padRight(2);
@@ -39,21 +42,30 @@ const unitSpawner = extendContent(Block, "unit-spawner", {
     configured(tile, player, value){
         //yes im terrible at this
         var handle = [
-           (tile) => this.pick(tile),
-           (tile) => {
-               var unit = tile.entity.unit().create(tile.entity.team());
-               unit.set(tile.entity.getX(), tile.entity.getY());
-               unit.add();
-           },
-           (tile) => tile.entity.setTeam(tile.entity.team() == Team.sharded ? Team.crux : Team.sharded)
+            (tile) => this.pick(tile),
+            (tile) => {
+                var unit = tile.entity.unit().create(tile.entity.team());
+                unit.set(tile.entity.getX(), tile.entity.getY());
+                unit.add();
+            },
+            (tile) => tile.entity.setTeam((tile.entity.team() + 1) > 5 ? 0 : tile.entity.team() + 1)
         ];
 
         handle[value](tile);
+    },
+
+    drawLayer(tile){
+        Draw.mixcol(Team.get(tile.entity.team()).color, 1.0);
+        Draw.rect(tile.entity.unit().icon(Cicon.medium), tile.drawx(), tile.drawy(), 5, 5);
+        Draw.mixcol();
+    },
+    draw(tile){
+        Draw.rect(this.regionDraw, tile.drawx(), tile.drawy(), 0);
     }
 });
 unitSpawner.entityType = prov(() => extend(TileEntity, {
     _unit: UnitTypes.dagger,
-    _team: Team.crux,
+    _team: 0,
 
     unit(){ return this._unit },
     setUnit(unit){ this._unit = unit },
@@ -63,6 +75,7 @@ unitSpawner.entityType = prov(() => extend(TileEntity, {
 }));
 
 unitSpawner.health = 1;
+unitSpawner.layer = Layer.overlay;
 unitSpawner.solid = false;
 unitSpawner.configurable = true;
 unitSpawner.buildVisibility = BuildVisibility.sandboxOnly;
