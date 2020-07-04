1c26833bfd5453e9c1a6f7e2be9d403c926de832
Sunny Kim <58885089+sk7725@users.noreply.github.com>
2020-07-04 12:35:19 +0900

    musich

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,44 @@
 require("community-mod/shaders");
 require("shaderwalls");
 
+const Music = Packages.arc.audio.Music;
+
+function _load(name, func) {
+	Core.assets.load("sounds/" + name + ".ogg", Music).loaded = cons(func);
+}
+
+function load(name, dark) {
+	_load(name, music => {
+		Vars.control.music[dark ? "darkMusic" : "ambientMusic"].add(music);
+	});
+}
+
+function loadTo(name, key) {
+	_load(name, music => {
+		Musics[key] = music;
+	});
+}
+
+function loaded() {
+	//Vars.control.music.darkMusic.clear();
+	Vars.control.music.ambientMusic.clear();
+
+	//loadTo("Endless - Keep Calm", "editor");
+	//load("Jasper Byrne - Decade Dance", true);
+	//load("Jasper Byrne - Voyager");
+	//load("Mega Drive - NARC", true);
+	load("h");
+	//load("MOON - Hydrogen", true);
+	//loadTo("MOON - Paris", "launch");
+	//loadTo("The Green Kingdom - Untitled", "menu");
+}
+
+if (Vars.ui.hudGroup) {
+	loaded();
+} else {
+	Events.on(EventType.ClientLoadEvent, run(loaded));
+}
+
 const ritzip = () => {
 const unitSpawner = extendContent(Block, "unit-spawner", {
     buildConfiguration(tile, table){
